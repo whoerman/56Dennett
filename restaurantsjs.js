0729df67a2f3649ed48faa5cd35ba960f5dad482
Walter Hoerman <whoerman@gmail.com>
2020-09-17 11:34:54 -0400

    changing button style

--- restaurantsjs.js
+++ restaurantsjs.js
@@ -22,11 +22,24 @@ let restaurantData = [
     tripadvisor: 'https://www.tripadvisor.com/Restaurant_Review-g46209-d20115690-Reviews-Rooftop_at_The_Envio-Portsmouth_New_Hampshire.html',
     directions: 'https://www.google.com/maps/dir/56+Dennett+St,+Portsmouth,+NH+03801,+USA/Rooftop+at+The+Envio,+Vaughan+Street,+Portsmouth,+NH/@43.0797615,-70.7674894,17z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x89e2bfa123522ebf:0x9d1eb0015bd0fbb0!2m2!1d-70.7681287!2d43.0795147!1m5!1m1!1s0x89e2bf9ad89e7f4d:0x5b17ccd8a59d5b4c!2m2!1d-70.762421!2d43.0797459!3e2',
 },
+{name: 'Green Elephant',
+    mode: 'Walkable',
+    distance: '0.4',
+    logo: 'greenelephantlogo.jpg',
+    summary: 'Vegetarian and Vegan',
+    website: 'https://www.greenelephantnh.com/',
+    yelp: 'https://www.yelp.com/biz/green-elephant-portsmouth',
+    tripadvisor: 'https://www.tripadvisor.com/Restaurant_Review-g46209-d8657507-Reviews-Green_Elephant-Portsmouth_New_Hampshire.html',
+    directions: 'https://www.google.com/maps/dir/56+Dennett+St,+Portsmouth,+NH+03801,+USA/Green+Elephant+Vegetarian+Bistro+%26+Bar,+Portwalk+Place,+Portsmouth,+NH/@43.0789114,-70.7669103,17z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x89e2bfa123522ebf:0x9d1eb0015bd0fbb0!2m2!1d-70.7681287!2d43.0795147!1m5!1m1!1s0x89e2bf0bba1d0bf1:0xcc537bb0eead071d!2m2!1d-70.7613527!2d43.0781121!3e2',
+},
 ];
 
 
+
+
+//restaurant card structrue
 for (i = 0; i < restaurantData.length; i++) {
-restarantCard = '<div class="card border-secondary text-center"> \
+currentRestarantCard = '<div class="card border-secondary text-center"> \
 <h5 class="text-center m-1"><i class="fas fa-walking"></i> '+ restaurantData[i].mode +' - '+ restaurantData[i].distance +' miles</h5> \
 <img class="card-img-top" src="./assets/images/restaurants/'+ restaurantData[i].logo +'" alt="'+ restaurantData[i].name +' restaurant logo"> \
     <div class="card-body"> \
@@ -37,6 +50,7 @@ restarantCard = '<div class="card border-secondary text-center"> \
         <a href="'+ restaurantData[i].directions +'" class="btn text-white bg-secondary m-1"><i class="fas fa-walking"></i> Walking directions</a> \
     </div> \
 </div>';
+//paste each iterated card into restaurant div
 document.getElementById("restaurantsdiv").innerHTML +=
-    restarantCard;
+    currentRestarantCard;
 }
