1ef2221d09b42a240e118594cd64f759ddbfee95
Walter Hoerman <whoerman@gmail.com>
2020-09-18 13:28:03 -0400

    pulling navbar into js

---
+++ navbar.js
@@ -0,0 +1,25 @@
+
+    let navheader = '<nav class="navbar navbar-light bg-light"> \
+<a href="https://www.56dennett.com"><span class="navbar-brand mb-0 h1"><i class="fas fa-home"></i> 56 Dennett <em>(Portsmouth, NH)</em> \
+    <Dlg></Dlg> \
+  </span></a> \
+<ul class="nav text-center"> \
+  <li class="nav-item"> \
+    <a class="nav-link" href="./index.html">Home</a> \
+  </li> \
+  <li class="nav-item"> \
+    <a class="nav-link" href="./parking.html">Park</a> \
+  </li> \
+  <li class="nav-item"> \
+    <a class="nav-link" href="./directions.html">Drive</a> \
+  </li> \
+  <li class="nav-item"> \
+    <a class="nav-link" href="./contact.html">Contact</a> \
+  </li> \
+</ul> \
+</nav>'
+
+
+
+document.getElementById("navbardiv").innerHTML +=
+navheader;
