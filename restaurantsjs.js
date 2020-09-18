8c63413a56bcf8c5e05121167656a774f138c7af
Walter Hoerman <whoerman@gmail.com>
2020-09-18 09:25:53 -0400

    adding restaurants

--- restaurantsjs.js
+++ restaurantsjs.js
@@ -1,56 +1,120 @@
-
 let restaurantCard = '';
 
-let restaurantData = [
-    {name: 'Barrio',
-    mode: 'Walkable',
-    distance: '0.3',
-    logo: 'barriologo.jpg',
-    summary: 'Tacos are their specialty',
-    website: 'https://barrio-tacos.com/location/barrio-portsmouth',
-    yelp: 'https://www.yelp.com/biz/barrio-portsmouth',
-    tripadvisor: 'https://www.tripadvisor.com/Restaurant_Review-g46209-d14784834-Reviews-Barrio-Portsmouth_New_Hampshire.html',
-    directions: 'https://www.google.com/maps/dir/56+Dennett+St,+Portsmouth,+NH+03801,+USA/Barrio,+Vaughan+Street,+Portsmouth,+NH/@43.0797615,-70.7677705,17z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x89e2bfa123522ebf:0x9d1eb0015bd0fbb0!2m2!1d-70.7681287!2d43.0795147!1m5!1m1!1s0x89e2bf098ccd0a59:0x638419366b50e4f6!2m2!1d-70.7629833!2d43.0799994!3e2',
-},
-    {name: 'Rooftop at Envio',
-    mode: 'Walkable',
-    distance: '0.3',
-    logo: 'enviologo.jpg',
-    summary: 'Drinks and Appetizers with a view of the city',
-    website: 'https://www.rooftopportsmouth.com/',
-    yelp: 'https://www.yelp.com/biz/rooftop-at-the-envio-portsmouth-2?osq=rooftop+at+envio',
-    tripadvisor: 'https://www.tripadvisor.com/Restaurant_Review-g46209-d20115690-Reviews-Rooftop_at_The_Envio-Portsmouth_New_Hampshire.html',
-    directions: 'https://www.google.com/maps/dir/56+Dennett+St,+Portsmouth,+NH+03801,+USA/Rooftop+at+The+Envio,+Vaughan+Street,+Portsmouth,+NH/@43.0797615,-70.7674894,17z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x89e2bfa123522ebf:0x9d1eb0015bd0fbb0!2m2!1d-70.7681287!2d43.0795147!1m5!1m1!1s0x89e2bf9ad89e7f4d:0x5b17ccd8a59d5b4c!2m2!1d-70.762421!2d43.0797459!3e2',
-},
-{name: 'Green Elephant',
-    mode: 'Walkable',
-    distance: '0.4',
-    logo: 'greenelephantlogo.jpg',
-    summary: 'Vegetarian and Vegan',
-    website: 'https://www.greenelephantnh.com/',
-    yelp: 'https://www.yelp.com/biz/green-elephant-portsmouth',
-    tripadvisor: 'https://www.tripadvisor.com/Restaurant_Review-g46209-d8657507-Reviews-Green_Elephant-Portsmouth_New_Hampshire.html',
-    directions: 'https://www.google.com/maps/dir/56+Dennett+St,+Portsmouth,+NH+03801,+USA/Green+Elephant+Vegetarian+Bistro+%26+Bar,+Portwalk+Place,+Portsmouth,+NH/@43.0789114,-70.7669103,17z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x89e2bfa123522ebf:0x9d1eb0015bd0fbb0!2m2!1d-70.7681287!2d43.0795147!1m5!1m1!1s0x89e2bf0bba1d0bf1:0xcc537bb0eead071d!2m2!1d-70.7613527!2d43.0781121!3e2',
-},
+let restaurantData = [{
+        name: 'Barrio',
+        mode: 'Walkable',
+        distance: '0.3',
+        logo: 'barriologo.jpg',
+        summary: 'Tacos are their specialty',
+        website: 'https://barrio-tacos.com/location/barrio-portsmouth',
+        yelp: 'https://www.yelp.com/biz/barrio-portsmouth',
+        tripadvisor: 'https://www.tripadvisor.com/Restaurant_Review-g46209-d14784834-Reviews-Barrio-Portsmouth_New_Hampshire.html',
+        directions: 'https://www.google.com/maps/dir/56+Dennett+St,+Portsmouth,+NH+03801,+USA/Barrio,+Vaughan+Street,+Portsmouth,+NH/@43.0797615,-70.7677705,17z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x89e2bfa123522ebf:0x9d1eb0015bd0fbb0!2m2!1d-70.7681287!2d43.0795147!1m5!1m1!1s0x89e2bf098ccd0a59:0x638419366b50e4f6!2m2!1d-70.7629833!2d43.0799994!3e2',
+    },
+    {
+        name: 'Rooftop at Envio',
+        mode: 'Walkable',
+        distance: '0.3',
+        logo: 'enviologo.jpg',
+        summary: 'Drinks and Appetizers with a view of the city',
+        website: 'https://www.rooftopportsmouth.com/',
+        yelp: 'https://www.yelp.com/biz/rooftop-at-the-envio-portsmouth-2?osq=rooftop+at+envio',
+        tripadvisor: 'https://www.tripadvisor.com/Restaurant_Review-g46209-d20115690-Reviews-Rooftop_at_The_Envio-Portsmouth_New_Hampshire.html',
+        directions: 'https://www.google.com/maps/dir/56+Dennett+St,+Portsmouth,+NH+03801,+USA/Rooftop+at+The+Envio,+Vaughan+Street,+Portsmouth,+NH/@43.0797615,-70.7674894,17z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x89e2bfa123522ebf:0x9d1eb0015bd0fbb0!2m2!1d-70.7681287!2d43.0795147!1m5!1m1!1s0x89e2bf9ad89e7f4d:0x5b17ccd8a59d5b4c!2m2!1d-70.762421!2d43.0797459!3e2',
+    },
+    {
+        name: 'Green Elephant',
+        mode: 'Walkable',
+        distance: '0.4',
+        logo: 'greenelephantlogo.jpg',
+        summary: 'Vegetarian and Vegan',
+        website: 'https://www.greenelephantnh.com/',
+        yelp: 'https://www.yelp.com/biz/green-elephant-portsmouth',
+        tripadvisor: 'https://www.tripadvisor.com/Restaurant_Review-g46209-d8657507-Reviews-Green_Elephant-Portsmouth_New_Hampshire.html',
+        directions: 'https://www.google.com/maps/dir/56+Dennett+St,+Portsmouth,+NH+03801,+USA/Green+Elephant+Vegetarian+Bistro+%26+Bar,+Portwalk+Place,+Portsmouth,+NH/@43.0789114,-70.7669103,17z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x89e2bfa123522ebf:0x9d1eb0015bd0fbb0!2m2!1d-70.7681287!2d43.0795147!1m5!1m1!1s0x89e2bf0bba1d0bf1:0xcc537bb0eead071d!2m2!1d-70.7613527!2d43.0781121!3e2',
+    },
+    {
+        name: '',
+        mode: 'Walkable',
+        distance: '0.7',
+        logo: 'durbarsquarelogo.jpg',
+        summary: 'Nepali Vegetarian Friendly Vegan Options',
+        website: 'http://www.durbarsquareportsmouth.com/',
+        yelp: 'https://www.yelp.com/biz/durbar-square-restaurant-portsmouth?osq=durbar+squaare',
+        tripadvisor: 'https://www.tripadvisor.com/Restaurant_Review-g46209-d2413812-Reviews-Durbar_Square_Restaurant-Portsmouth_New_Hampshire.html',
+        directions: 'https://www.google.com/maps/dir/56+Dennett+St,+Portsmouth,+NH+03801,+USA/Durbar+Square+Restaurant,+Market+Street,+Portsmouth,+NH/@43.0778605,-70.7672364,16z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x89e2bfa123522ebf:0x9d1eb0015bd0fbb0!2m2!1d-70.7681287!2d43.0795147!1m5!1m1!1s0x89e2bf0c6da6ed89:0x836ad8e345857cf3!2m2!1d-70.757577!2d43.0773378!3e2',
+    },
+    {
+        name: '',
+        mode: 'Walkable',
+        distance: '',
+        logo: '',
+        summary: '',
+        website: '',
+        yelp: '',
+        tripadvisor: '',
+        directions: '',
+    }, {
+        name: '',
+        mode: 'Walkable',
+        distance: '',
+        logo: '',
+        summary: '',
+        website: '',
+        yelp: '',
+        tripadvisor: '',
+        directions: '',
+    }, {
+        name: '',
+        mode: 'Walkable',
+        distance: '',
+        logo: '',
+        summary: '',
+        website: '',
+        yelp: '',
+        tripadvisor: '',
+        directions: '',
+    },
 ];
 
+<div class="card border-secondary text-center">
+<h5 class="text-center m-1"><i class="fas fa-walking"></i> Walkable - 0.8 miles</h5>
+<img class="card-img-top" src="./assets/images/restaurants/massimologo.jpg"
+    alt="restaurante massimo logo">
+<div class="card-body">
+    <h5 class="card-title">Italian</h5>
+    <a href="https://ristorantemassimo.com/" class="btn btn-warning m-1"><i class="fas fa-utensils"></i>
+        Restaurante Massimo website</a>
+    <a href="https://www.tripadvisor.com/Restaurant_Review-g46209-d659447-Reviews-Ristorante_Massimo-Portsmouth_New_Hampshire.html"
+        class="btn btn-success m-1"><i class="fab fa-tripadvisor"></i> Tripadvisor</a>
+    <a href="https://www.google.com/maps/dir/56+Dennett+St,+Portsmouth,+NH+03801,+USA/Massimo's+-+Ristorante+Massimo,+Penhallow+Street,+Portsmouth,+NH/@43.0776389,-70.7665442,16z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x89e2bfa123522ebf:0x9d1eb0015bd0fbb0!2m2!1d-70.7681287!2d43.0795147!1m5!1m1!1s0x89e2bf0c3818b215:0xf0bf1bca27cab65!2m2!1d-70.7561424!2d43.0771685!3e2"
+        class="btn text-white bg-secondary m-1"><i class="fas fa-walking"></i> Walking directions</a>
+</div>
+</div>
 
 
 
-//restaurant card structrue
-for (i = 0; i < restaurantData.length; i++) {
-currentRestarantCard = '<div class="card border-secondary text-center"> \
-<h5 class="text-center m-1"><i class="fas fa-walking"></i> '+ restaurantData[i].mode +' - '+ restaurantData[i].distance +' miles</h5> \
-<img class="card-img-top" src="./assets/images/restaurants/'+ restaurantData[i].logo +'" alt="'+ restaurantData[i].name +' restaurant logo"> \
+
+
+
+function makeCard() {
+    currentRestarantCard = '<div class="card border-secondary text-center"> \
+<h5 class="text-center m-1"><i class="fas fa-walking"></i> ' + restaurantData[i].mode + ' - ' + restaurantData[i].distance + ' miles</h5> \
+<img class="card-img-top" src="./assets/images/restaurants/' + restaurantData[i].logo + '" alt="' + restaurantData[i].name + ' restaurant logo"> \
     <div class="card-body"> \
-        <h5 class="card-title">'+ restaurantData[i].summary +'</h5> \
-        <a href="'+ restaurantData[i].website +'" class="btn btn-warning m-1"><i class="fas fa-utensils"></i> '+ restaurantData[i].name +' website</a> \
-        <a href="'+ restaurantData[i].yelp +'" class="btn btn-danger m-1"><i class="fab fa-yelp"></i> Yelp</a> \
-        <a href="'+ restaurantData[i].tripadvisor +'" class="btn btn-success m-1"><i class="fab fa-tripadvisor"></i> Tripadvisor</a> \
-        <a href="'+ restaurantData[i].directions +'" class="btn text-white bg-secondary m-1"><i class="fas fa-walking"></i> Walking directions</a> \
+        <h5 class="card-title">' + restaurantData[i].summary + '</h5> \
+        <a href="' + restaurantData[i].website + '" class="btn btn-warning m-1"><i class="fas fa-utensils"></i> ' + restaurantData[i].name + ' website</a> \
+        <a href="' + restaurantData[i].yelp + '" class="btn btn-danger m-1"><i class="fab fa-yelp"></i> Yelp</a> \
+        <a href="' + restaurantData[i].tripadvisor + '" class="btn btn-success m-1"><i class="fab fa-tripadvisor"></i> Tripadvisor</a> \
+        <a href="' + restaurantData[i].directions + '" class="btn text-white bg-secondary m-1"><i class="fas fa-walking"></i> Walking directions</a> \
     </div> \
 </div>';
-//paste each iterated card into restaurant div
-document.getElementById("restaurantsdiv").innerHTML +=
-    currentRestarantCard;
+}
+
+//restaurant card structure
+for (i = 0; i < restaurantData.length; i++) {
+    makeCard();
+    //paste each iterated card into restaurant div
+    document.getElementById("restaurantsdiv").innerHTML +=
+        currentRestarantCard;
 }
