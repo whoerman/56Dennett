83c4fd32b2a230f8f3aef15d0d74af97c1aa6916
Walter Hoerman <whoerman@gmail.com>
2021-08-23 18:27:07 -0400

    adding home owner matrix

---
+++ ownerList.js
@@ -0,0 +1,202 @@
+let ownerListArray = [
+    {
+        owner: 'Ephraim Dennett',
+        monthBought: null,
+        yearBought: 1730,
+        monthSold: null,
+        yearSold: 1741,
+        source: 'Dennett Family Lineage',
+        bio: "Ephraim was born on 1689-08-02. He died in 1741 at the age of 51 years. Ephraim was the son of Dennett, John and Sherburne, Amy. He married , Catherine. The youngest child of John and Amy was Ephraim, who was born Aug. 2, 1689, and lived with his mother until she died. He controlled the land on the main property along today's roadway, Dennett Street. Ephraim Dennett became the most politically active of the Dennetts. He served as a selectman from 1715 to 1718 and representative to the legislative house from 1718 to 1728. Newington was separated from Porstmouth in 1713, and Ephraim's land was on the edge of Portsmouth. In 1732, Ephraim was appointed to the Governor's Council at the time that Richard Waldron controlled politics, from the death of Lt. Governor John Wentworth until Benning Wentworth was named Royal Governor of the 'old colony. Ephraim Sr. married Catherine and had at least five children, of whom Ephraom Jr, born July 22, 1718 was the eldest."
+    },
+    {
+        owner: 'John Dennett',
+        monthBought: null,
+        yearBought: 1741,
+        monthSold: null,
+        yearSold: 1787,
+        source: 'Dennett Family Lineage',
+        bio: 'John was born on 1716-10-15 in New Hampshire. He died in 1787 in New Hampshire at the age of 70 years. John was the son of Dennett, Ephraim and , Catherine. He married Bartlett, Phobe in 1739-02-00.'
+    },
+    {
+        owner: 'Nathaniel Dennett',
+        monthBought: null,
+        yearBought: 1787,
+        monthSold: null,
+        yearSold: 1808,
+        source: 'Dennett Family Lineage',
+        bio: null
+    },
+    {
+        owner: 'Ephraim Dennett',
+        monthBought: null,
+        yearBought: 1808,
+        monthSold: null,
+        yearSold: 1832,
+        source: 'Dennett Family Lineage',
+        bio: null
+    },
+    {
+        owner: 'George W, Dennett',
+        monthBought: null,
+        yearBought: 1832,
+        monthSold: "June",
+        yearSold: 1866,
+        source: 'Dennett Family Lineage',
+        bio: null
+    },
+    {
+        owner: 'George and Ellen Clark',
+        monthBought: "June",
+        yearBought: 1866,
+        monthSold: "March",
+        yearSold: 1882,
+        source: 'Rockingham County Deeds',
+        bio: null
+    },
+    {
+        owner: 'Mary and Clarence Nutter',
+        monthBought: "March",
+        yearBought: 1882,
+        monthSold: "September",
+        yearSold: 1913,
+        source: 'Rockingham County Deeds',
+        bio: null
+    },
+    {
+        owner: 'Ida Seymour',
+        monthBought: "September",
+        yearBought: 1913,
+        monthSold: "July",
+        yearSold: 1919,
+        source: 'Rockingham County Deeds',
+        bio: null
+    },
+    {
+        owner: 'Plummer B. Smith',
+        monthBought: "July",
+        yearBought: 1919,
+        monthSold: "June",
+        yearSold: 1941,
+        source: 'Rockingham County Deeds',
+        bio: "In 1905, Plummer Smith was a painter at the Navy Yard."
+    },
+    {
+        owner: 'Ruth Chase',
+        monthBought: "June",
+        yearBought: 1941,
+        monthSold: "April",
+        yearSold: 1945,
+        source: 'Rockingham County Deeds',
+        bio: null
+    },
+    {
+        owner: 'Dorothy Hart',
+        monthBought: "April",
+        yearBought: 1945,
+        monthSold: "September",
+        yearSold: 1956,
+        source: 'Rockingham County Deeds',
+        bio: null
+    },
+    {
+        owner: 'Elizabeth Anne Virgil',
+        monthBought: "September",
+        yearBought: 1956,
+        monthSold: "February",
+        yearSold: 1971,
+        source: 'Rockingham County Deeds',
+        bio: 'The year was 1926. UNHs senior class posed in front of Hamilton Smiths white pillars as a photographer took their picture for the yearbook. Near the center, surrounded by classmates with bobbed hair, sat Elizabeth Ann Virgil 26, who a few months later would be the first African-American to earn a bachelors degree from UNH. Thus did the university become desegregated: quietly, uneventfully, without fanfare or noticeable dissent. In 1951, she joined UNH, where she typed reports from data collected by soil conservation scientists. She found the work interesting--especially later on when women were allowed out into the field--and she stayed for 22 years.'
+    },
+    {
+        owner: 'Bernard Hiatt',
+        monthBought: "February",
+        yearBought: 1971,
+        monthSold: "August",
+        yearSold: 1975,
+        source: 'Rockingham County Deeds',
+        bio: null
+    },
+    {
+        owner: 'Robert and Helen Dysinger',
+        monthBought: "August",
+        yearBought: 1975,
+        monthSold: "November",
+        yearSold: 1976,
+        source: 'Rockingham County Deeds',
+        bio: null
+    },
+    {
+        owner: 'Stockton and Elizabeth Warnock',
+        monthBought: "November",
+        yearBought: 1976,
+        monthSold: "October",
+        yearSold: 1977,
+        source: 'Rockingham County Deeds',
+        bio: null
+    },
+    {
+        owner: 'John and Joan Rice',
+        monthBought: "October",
+        yearBought: 1977,
+        monthSold: "December",
+        yearSold: 1985,
+        source: 'Rockingham County Deeds',
+        bio: null
+    },
+    {
+        owner: 'Scott and MaryBeth and Stanley McDonald',
+        monthBought: "December",
+        yearBought: 1985,
+        monthSold: "July",
+        yearSold: 1987,
+        source: 'Rockingham County Deeds',
+        bio: null
+    },
+    {
+        owner: 'William and Eleanor Tracy',
+        monthBought: "July",
+        yearBought: 1987,
+        monthSold: "March",
+        yearSold: 2009,
+        source: 'Rockingham County Deeds',
+        bio: null
+    },
+    {
+        owner: 'Robert and Eileen Mackin',
+        monthBought: "March",
+        yearBought: 2009,
+        monthSold: "March",
+        yearSold: 2017,
+        source: 'Rockingham County Deeds',
+        bio: null
+    },
+    {
+        owner: 'Walter and Mary Ellen Hoerman',
+        monthBought: "March",
+        yearBought: 2017,
+        monthSold: null,
+        yearSold: null,
+        source: 'Rockingham County Deeds',
+        bio: null
+    },
+
+];
+
+//javascript file for cards for all restaurants 
+listOwners();
+
+//restaurant card structure
+function listOwners() {
+    for (i = 0; i < (ownerListArray.length); i++) {
+        console.log(i);
+        console.log(ownerListArray[i].owner)
+        document.getElementById("56Owners").innerHTML +=
+        '<div class="card border-secondary text-center"> \
+        <h6 class="text-center font-italic m-1"></h6> \
+        <h6 class="text-center font-italic m-1"> From ' + (ownerListArray[i].yearBought) + ' to ' + (ownerListArray[i].yearSold) + '</h6> \
+        <h4 class="text-center m-1">' + (ownerListArray[i].owner) + '</h4> \
+        <h6 class="text-center font-italic m-1"></h6> \
+        </div>'
+        ;
+    }
+}
